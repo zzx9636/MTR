85c52b6df17f624adac770690cd5281d6eccc4a3
kaichiehhsu <kaichieh@princeton.edu>
2023-10-30 16:31:30 -0400

    script

--- tools/mtr_lightning.py
+++ tools/mtr_lightning.py
@@ -11,17 +11,17 @@ from mtr.models import model as model_utils
 import lightning.pytorch as pl
 from lightning.pytorch.callbacks import ModelCheckpoint, LearningRateMonitor
 from lightning.pytorch.loggers import WandbLogger
-
-
 from easydict import EasyDict
+
 
 class PrintLogger():
     @staticmethod
     def info(msg: str):
         print(msg)
 
+
 class MTR_Lightning(pl.LightningModule):
-    def __init__(self, cfg: EasyDict, logger = None, 
+    def __init__(self, cfg: EasyDict, logger=None,
                  pretrained_model: str = None, freeze_pretrained: bool = False) -> None:
         super().__init__()
         self.save_hyperparameters(ignore=['logger', 'pretrained_model', 'freeze_pretrained'])
@@ -29,40 +29,40 @@ class MTR_Lightning(pl.LightningModule):
         self.cfg = cfg
         self.opt_cfg = cfg.OPTIMIZATION
         self.epoch = 0
-        self.model = model_utils.MotionTransformer(config = cfg.MODEL)
-        
+        self.model = model_utils.MotionTransformer(config=cfg.MODEL)
+
         if pretrained_model is not None:
             self.model.load_params_from_file(
                 filename=pretrained_model,
                 logger=logger,
-                freeze_pretrained = freeze_pretrained,
-                keys_to_ignore = 'motion_decoder'
+                freeze_pretrained=freeze_pretrained,
+                keys_to_ignore='motion_decoder'
             )
-        
+
     def configure_optimizers(self):
         '''
         This function is called by Lightning to create the optimizer and learning rate scheduler.
         '''
-        
+
         param2opt = [param for param in self.model.parameters() if param.requires_grad]
         # print('motion_decoder.query' in param2opt)
         # print(param2opt)
         if self.opt_cfg.OPTIMIZER == 'Adam':
             optimizer = torch.optim.Adam(
                 param2opt,
-                lr=self.opt_cfg.LR, 
+                lr=self.opt_cfg.LR,
                 weight_decay=self.opt_cfg.get('WEIGHT_DECAY', 0)
             )
         elif self.opt_cfg.OPTIMIZER == 'AdamW':
             optimizer = torch.optim.AdamW(
-                param2opt, 
+                param2opt,
                 lr=self.opt_cfg.LR,
                 weight_decay=self.opt_cfg.get('WEIGHT_DECAY', 0))
         else:
             assert False
-            
+
         # Set up the learning rate scheduler
-        
+
         last_epoch = self.epoch-1
         if self.opt_cfg.get('SCHEDULER', None) == 'cosine':
             scheduler = lr_sched.CosineAnnealingWarmRestarts(
@@ -74,7 +74,7 @@ class MTR_Lightning(pl.LightningModule):
             )
         elif self.opt_cfg.get('SCHEDULER', None) == 'lambdaLR':
             total_iters_each_epoch = self.opt_cfg.DATA_LOADER_SIZE // self.opt_cfg.BATCH_SIZE_PER_GPU
-            
+
             decay_steps = [x * total_iters_each_epoch for x in self.opt_cfg.get('DECAY_STEP_LIST', [5, 10, 15, 20])]
             def lr_lbmd(cur_epoch):
                 cur_decay = 1
@@ -82,7 +82,7 @@ class MTR_Lightning(pl.LightningModule):
                     if cur_epoch >= decay_step:
                         cur_decay = cur_decay * self.opt_cfg.LR_DECAY
                 return max(cur_decay, self.opt_cfg.LR_CLIP / self.opt_cfg.LR)
-            
+
             scheduler = lr_sched.LambdaLR(optimizer, lr_lbmd, last_epoch=last_epoch)
         elif self.opt_cfg.get('SCHEDULER', None) == 'linearLR':
             total_iters = total_iters_each_epoch * self.opt_cfg.NUM_EPOCHS
@@ -95,33 +95,33 @@ class MTR_Lightning(pl.LightningModule):
             )
         else:
             scheduler = None
-        
+
         if scheduler is not None:
             return [optimizer], [{"scheduler": scheduler, "interval": "step"}]
         else:
             return optimizer
-        
+
     def training_step(self, batch, batch_idx):
         batch_size = len(batch['input_dict']['track_index_to_predict'])
         output = self.model(batch)
         loss = output[0]
         tb_dict = output[1]
-        
+
         log_dict = {f'train/{k}': v for k, v in tb_dict.items()}
         self.log_dict(log_dict, on_step=True, prog_bar=True, logger=True, batch_size=batch_size)
-        
+
         return loss
-    
+
     def validation_step(self, batch, batch_idx):
         batch_size = len(batch['input_dict']['track_index_to_predict'])
         output = self.model(batch)
         tb_dict = output[1]
         log_dict = {f'val/{k}': v for k, v in tb_dict.items()}
         self.log_dict(log_dict, on_step=False, prog_bar=True, logger=True, batch_size=batch_size)
-        
+
     def forward(self, batch, get_loss: bool = False):
         return self.model(batch, get_loss)
-    
+
     def sample(self, batch, layer = -1):
         self.eval()
         with torch.no_grad():
@@ -138,80 +138,77 @@ class MTR_Lightning(pl.LightningModule):
         #     sample = gmm.sample()#.cpu().numpy()
         #     sample = (sample * self.model.motion_decoder.output_std + self.model.motion_decoder.output_mean).cpu().numpy()
         # return mode, mix, gmm, sample
-        
-        
-        
+
+
 # main function
 def train(cfg_file, pretrained_model, freeze_pretrained):
     logger = PrintLogger()
     # Load the config
     cfg_from_yaml_file(cfg_file, cfg)
-    
+
     batch_size = cfg.OPTIMIZATION.BATCH_SIZE_PER_GPU
     epochs = cfg.OPTIMIZATION.NUM_EPOCHS
-    
+
     # Construct Dataset
     train_dataset = WaymoDataset(
         dataset_cfg=cfg.DATA_CONFIG,
         training=True,
-        logger=logger, 
+        logger=logger,
     )
-    
+
     val_dataset = WaymoDataset(
         dataset_cfg=cfg.DATA_CONFIG,
         training=False,
-        logger=logger, 
+        logger=logger,
     )
-    
+
     cfg.OPTIMIZATION.DATA_LOADER_SIZE = len(train_dataset)
-    
+
     train_loader = DataLoader(
         train_dataset, batch_size=batch_size, pin_memory=True, num_workers=8,
         shuffle=True, collate_fn=train_dataset.collate_batch,
         drop_last=False
     )
-    
+
     val_loader = DataLoader(
         val_dataset, batch_size=batch_size, pin_memory=True, num_workers=8,
         shuffle=False, collate_fn=val_dataset.collate_batch,
         drop_last=False
     )
-    
+
     model = MTR_Lightning(cfg, logger, pretrained_model, freeze_pretrained)
-    
-    logger = WandbLogger(project='MTR_BC_ATTEN', entity='zzx9636', log_model = True)
+
+    logger = WandbLogger(project='lpp', entity='kaichiehhsu', log_model=True)
     logger.watch(model, log_freq=100)
-    
+
     # logger = None
     num_decoder = cfg.MODEL.MOTION_DECODER.NUM_DECODER_LAYERS
     freeze_str = 'freeze' if freeze_pretrained else 'unfreeze'
-    
+
     trainer = pl.Trainer(
         max_epochs=epochs,
         accelerator="gpu",
-        enable_progress_bar=True, 
-        logger=logger, 
+        enable_progress_bar=True,
+        logger=logger,
         detect_anomaly=True,
         gradient_clip_val=0.5, gradient_clip_algorithm="value",
         callbacks=[
             ModelCheckpoint(
-                dirpath = f'output/bc_atten_{num_decoder}_{freeze_str}',
+                dirpath=f'output/heatmap_bc_atten_{num_decoder}_{freeze_str}_ce',
                 save_top_k=10,
                 save_last=True,
-                monitor='val/loss_total', 
+                monitor='val/loss_total',
                 save_on_train_epoch_end=True
             ),
             LearningRateMonitor(logging_interval='step')
         ]
     )
-    
+
     trainer.fit(model, train_loader, val_loader)
-    
+
 if __name__ == '__main__':
     train(
-        'tools/cfgs/waymo/bc+10_percent_data_atten.yaml',
-        'model/checkpoint_epoch_30.pth',
+        '/home/kai/Desktop/mtr/tools/cfgs/waymo/heatmap_bc+10_percent_data.yaml',
+        '/Data/checkpoint_epoch_30.pth',
         True
     )
-
-         
